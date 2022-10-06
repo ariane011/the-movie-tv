4caac9dc9671f2838ae2ef0e29716d09695962b3
Ariane Sousa <ariane.sousa011@gmail.com>
2022-10-06 04:12:43 -0300

    Layout página do filme

--- src/pages/MoviesDetails/MoviesDetails.styled.js
+++ src/pages/MoviesDetails/MoviesDetails.styled.js
@@ -6,4 +6,56 @@ export const StyleHeader = styled.div`
   height: 100px;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  .img-background {
+    width: 100%;
+    background-size: cover;
+    -webkit-mask-image: linear-gradient(
+      to bottom,
+      #a422283b 25%,
+      #d9363e00 100%
+    );
+  }
+  .div-img-bg img {
+    position: relative;
+  }
+  .div-img-bg {
+    background: black;
+  }
+  .div-infos {
+    display: flex;
+    position: absolute;
+    top: 0px;
+    img {
+      width: 300px;
+      margin: 150px 50px 50px 50px;
+      border-radius: 10px;
+    }
+    .div-details {
+      color: whitesmoke;
+      margin-top: 150px;
+      font-family: "Roboto", sans-serif;
+    }
+    h1 {
+      color: whitesmoke;
+      font-family: "Roboto", sans-serif;
+      font-weight: 700;
+      font-size: 28px;
+    }
+    h2 {
+      color: whitesmoke;
+      font-family: "Roboto", sans-serif;
+      font-weight: 700;
+      font-size: 20px;
+    }
+    .txt-tagline {
+      font-style: italic;
+    }
+    .txt-overview {
+      width: 600px;
+    }
+    p {
+      font-size: medium;
+    }
+  }
+`;
--- src/pages/MoviesDetails/index.js
+++ src/pages/MoviesDetails/index.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, message } from "antd";
+import { message } from "antd";
 import { Container, StyleHeader } from "./MoviesDetails.styled";
 import { MovieDetails } from "../../service/MovieDetails";
 import { useParams } from "react-router-dom";
+import moment from "moment";
 
 export const MoviesDetails = () => {
   const [movie, setMovie] = useState("");
   const { movieId } = useParams();
+
+  function percentage(num, per) {
+    return (num / 100) * per;
+  }
 
   useEffect(() => {
     try {
@@ -25,26 +30,34 @@ export const MoviesDetails = () => {
     <>
       <StyleHeader />
       <Container>
-        {/* background image */}
-        <img
-          src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
-          alt={movie.title}
-        />
-        <img
-          src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-          alt={movie.title}
-        />
-        <h1>{movie.title}</h1>
-        <p>{movie.release_date}</p>
-        <p>{movie.runtime}</p>
-        <p>{movie.vote_average}</p>
-        <p>Reproduzir trailer</p>
-        <p>{movie.tagline}</p>
-        <h2>Sinopse</h2>
-        <p>{movie.overview}</p>
-        <p>{movie.overview}</p>
-        <p>{movie.overview}</p>
-        <p>{movie.overview}</p>
+        <div className="div-img-bg">
+          <img
+            src={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+            alt={movie.title}
+            className="img-background"
+          />
+        </div>
+        <div className="div-infos">
+          <div>
+            <img
+              src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+              alt={movie.title}
+            />
+          </div>
+          <div className="div-details">
+            <h1>{movie.title}</h1>
+            <p style={{ fontWeight: 500 }}>
+              {moment(movie.release_date).format("DD/MM/YYYY")}
+            </p>
+            <p style={{ fontWeight: 500 }}>
+              Avaliação dos usuários:{" "}
+              {percentage(1000, movie.vote_average).toPrecision(2)}%
+            </p>
+            <p className="txt-tagline">{movie.tagline}</p>
+            <h2>Sinopse</h2>
+            <p className="txt-overview">{movie.overview}</p>
+          </div>
+        </div>
         {console.log(movie)}
       </Container>
     </>
